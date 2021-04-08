62481ac31b95284221fd78e4cf75e09ca20a203a
Mackan <76006738+MrMackan@users.noreply.github.com>
2021-04-08 11:43:45 +0200

    Trying to connect to DB
    Not working completely yet, pushing to allow more testing

--- WeatherLogger/src/main/java/poseidon/WeatherApp.java
+++ WeatherLogger/src/main/java/poseidon/WeatherApp.java
@@ -1,7 +1,10 @@
 package poseidon;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
  * WeatherApp is the main Application for start up.
@@ -14,5 +17,15 @@ public class WeatherApp {
     public static void main(String[] args) {
         var app = new SpringApplication(WeatherApp.class);
         app.run(args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        String sql = "INSERT INTO Test (Time, Data) VALUES ('ss', 'ja')";
+
+       int rows = jdbcTemplate.update(sql);
+       if (rows>0) {
+           System.out.println("new row created");
+       }
     }
 }
