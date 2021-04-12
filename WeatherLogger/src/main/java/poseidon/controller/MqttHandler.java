8753529c34e72df8e1d7b33160a3ea9a15861fdf
SpaceCrab <10452861+SpaceCrab@users.noreply.github.com>
2021-04-12 17:42:01 +0200

    Fix dependency issues
    added Jaxb-api in pom.xml

--- WeatherLogger/src/main/java/poseidon/controller/MqttHandler.java
+++ WeatherLogger/src/main/java/poseidon/controller/MqttHandler.java
@@ -1,10 +1,8 @@
 package poseidon.controller;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 import java.beans.*;
-import org.aspectj.bridge.IMessageHandler;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
@@ -13,8 +11,6 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.yaml.snakeyaml.Yaml;
 
-import net.bytebuddy.asm.Advice.This;
-import poseidon.controller.WeatherController;
 /**
  * @author Eric Lundin
  * @version 1.0.0
@@ -25,8 +21,6 @@ public class MqttHandler implements MqttCallback {
     private String subscription;
     private String username;
     private String password;
-
-    private PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
 
     public MqttHandler() {
         startClient();
@@ -65,10 +59,6 @@ public class MqttHandler implements MqttCallback {
         }
     }
 
-    public void addPropertychangeListener(PropertyChangeListener listener){
-        changeSupport.addPropertyChangeListener(listener);
-    }
-
     @Override
     public void connectionLost(Throwable arg0) {
         // TODO Auto-generated method stub
@@ -84,7 +74,6 @@ public class MqttHandler implements MqttCallback {
         // TODO Auto-generated method stub
         System.out.println(arg0);
         System.out.println(arg1);
-        changeSupport.firePropertyChange("propertyName", "", arg1);
     }
 
     private static MqttConnectOptions setUpConnectOptions(String userName, String password) {
