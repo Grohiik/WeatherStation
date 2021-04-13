db20773128ac9b249b5ae03cf07c0744460d9324
Ademir zjajo <ameswe@hotmail.se>
2021-04-13 11:25:50 +0200

    Updated graf
    Co-authored-by: CHeister27 <76813974+cheister27@users.noreply.github.com>

--- WebApp/src/scripts/app.ts
+++ WebApp/src/scripts/app.ts
@@ -12,13 +12,14 @@ async function getData(){
 
   const table = data.split('\n').slice(2)
 
-  table.forEach(row => {
+  rows.forEach(row => {2 
     const cols = row.split(',')
     const year = cols[0]
     xlabels.push(year)
     const temp = cols[1]
-    ytemps.push(Number (temp) + 14)
-    //console.log(year, temp)
+    //ytemps.push(Number(temp)) // detta är från 0 linjen 
+    ytemps.push(parseFloat(temp) + 14) // lägger till 14 för att få bättre graff
+    console.log(year, temp)
   })
 }
 
@@ -28,30 +29,44 @@ async function chartIt() {
   const myChart = new Chart(ctx, {
     type: 'line',
     data: {
+      //labels: ['1','2','3','4','5'],
       labels: xlabels,
       datasets: [{
         label: 'Global Average Temperature in C°, January',
         data: ytemps,
+        fill:true,    // detta är skuggan under grafe 
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
-        ],
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',        ],
         borderColor: [
           'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
         ],
-        borderWidth: 1
-      },]
+        borderWidth: 2 // tjokleken på linjen 
+      }]
     },
     options: {
       scales: {
-        y: {
-          ticks: {
-            callback: function(value, index, values) {
-              return value + '°'
-            }
-          }   
-        }
+          y: {
+              ticks: {
+                  // Include a dollar sign in the ticks
+                  callback: function(value, index, values) {
+                      return value + '°';
+                  }
+              }
+          }
       }
-    }
+  }
   })
 } 
+
+
 
