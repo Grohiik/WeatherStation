0e26a8b143afa85f51e10f8904bacdbf03c9ca87
KTheXIII <pratchaya.k.git@gmail.com>
2021-04-14 11:45:22 +0200

    Bring back HelloController.java

--- WeatherLogger/src/main/java/poseidon/controller/HelloController.java
+++ WeatherLogger/src/main/java/poseidon/controller/HelloController.java
@@ -1,27 +1,27 @@
-//package poseidon.controller;
-//
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
-//
-///**
-// * HelloController is a sample class for REST-API and its root path.
-// *
-// * @author Pratchaya Khansomboon
-// * @version 0.0.0
-// */
-//@RestController
-//    @RequestMapping(value = "api")
-//public class HelloController {
-//    @GetMapping("")
-//    String root() {
-//        return "Hello, World!";
-//    }
-//
-//    @GetMapping("/hello-there")
-//    String hello(@RequestParam(required = false) String name) {
-//        if (name != null) return "General " + name + "!";
-//        return "General Kenobi!";
-//    }
-//}
+package poseidon.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * HelloController is a sample class for REST-API and its root path.
+ *
+ * @author Pratchaya Khansomboon
+ * @version 0.0.0
+ */
+@RestController
+@RequestMapping(value = "api")
+public class HelloController {
+    @GetMapping("")
+    String root() {
+        return "Hello, World!";
+    }
+
+    @GetMapping("/hello-there")
+    String hello(@RequestParam(required = false) String name) {
+        if (name != null) return "General " + name + "!";
+        return "General Kenobi!";
+    }
+}
