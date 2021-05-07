8fc1b45f78ef009ca12d8c6eecf584574a9be212
Gabriell-a <81756064+Gabriell-a@users.noreply.github.com>
2021-05-07 09:33:43 +0200

    Cleaned up the code in app.ts

--- WebApp/src/scripts/app.ts
+++ WebApp/src/scripts/app.ts
@@ -21,7 +21,6 @@ import {
   updateChart,
   IDataset,
 } from './graph'
-import { Chart } from 'chart.js'
 
 async function main() {
   let deviceList: IDevice[] = []
@@ -38,29 +37,6 @@ async function main() {
 
   const canvas = createCanvas(mainView)
   const graph = createLineGraph(canvas)
-
-  const dataset = {
-    data: [0, 3, 5, 6, 7],
-    label: 'inte ademir',
-    borderColor: '#ff0f0f',
-  }
-  const dataset2 = {
-    data: [1, 9, -1, 6],
-    label: 'christian',
-    borderColor: '#ffff00',
-  }
-  const dataset3 = {
-    data: [-19, 1.54, -33, 6],
-    label: 'send help',
-    borderColor: '#00ff00',
-  }
-  addChartLabel(graph, '1')
-  addChartLabel(graph, '2')
-  addChartLabel(graph, '3')
-  addChartLabel(graph, '4')
-  // addChartDataset(graph, dataset)
-  // addChartDataset(graph, dataset2)
-  // addChartDataset(graph, dataset3)
 
   dropdownDevice.addEventListener('change', onDeviceSelect)
   dropdownData.addEventListener('change', onDeviceContentSelect)
@@ -86,27 +62,27 @@ async function main() {
 
   function onDeviceContentSelect() {
     currentContent = deviceContentList[dropdownData.selectedIndex - 1]
-    getSpecificData(currentDevice.device, currentContent.name)
-      .then(datas => {
-        currentData = datas
-        const dataset: IDataset = {
-          data: [],
-          label: '',
-          borderColor: '#ff0000'
-        }
-        const labels: string[] = []
-        clearChartData(graph)
+    getSpecificData(currentDevice.device, currentContent.name).then((datas) => {
+      currentData = datas
+      const dataset: IDataset = {
+        data: [],
+        label: '',
+        borderColor: '#ff0000',
+      }
+      const labels: string[] = []
+      clearChartData(graph)
 
-        for (let i = 0; i < currentData.length; i++) {
-          const date = new Date(Number(currentData[i].created) * 1000)
-          labels.push(date.toLocaleString('se-SE', { timeZone: 'UTC' }))
-          dataset.data.push(Number(currentData[i].value))
-        }
+      for (let i = 0; i < currentData.length; i++) {
+        const date = new Date(Number(currentData[i].created) * 1000)
+        labels.push(date.toLocaleString('se-SE', { timeZone: 'UTC' }))
+        dataset.data.push(Number(currentData[i].value))
+      }
 
-        setChartLabels(graph, labels)
-        addChartDataset(graph, dataset)
-        updateChart(graph)
-      })
+      setChartLabels(graph, labels)
+      addChartDataset(graph, dataset)
+      updateChart(graph)
+    })
+    clearChartData(graph)
   }
 }
 
