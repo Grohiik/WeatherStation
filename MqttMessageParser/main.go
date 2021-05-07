a378714e5a29fa6f78868335de0567f456801d17
SpaceCrab <10452861+SpaceCrab@users.noreply.github.com>
2021-05-07 16:48:23 +0200

    MqttMessageParser in GO
    Ported MqttHandler to GO to increase performance and decrease server
    load

--- MqttMessageParser/main.go
+++ MqttMessageParser/main.go
@@ -5,10 +5,6 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
-
-	"MqttMessageParser/model"
-	"MqttMessageParser/parser"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/jinzhu/gorm"
@@ -19,26 +15,19 @@ import (
 var db *gorm.DB //database
 
 func init() {
-	//supernecessary ascii art
-	fmt.Printf(`______  ______  _____________________   __
-___  / / /_  / / /_  ____/___  _/__  | / /
-__  /_/ /_  / / /_  / __  __  / __   |/ / 
-_  __  / / /_/ / / /_/ / __/ /  _  /|  /  
-/_/ /_/  \____/  \____/  /___/  /_/ |_/   
-`)
 
 	e := godotenv.Load() //Load .env file
 	if e != nil {
 		fmt.Print(e)
 	}
 
-	//get the connect variable for the database from the .env file
 	username := os.Getenv("db_user")
 	password := os.Getenv("db_pass")
 	dbName := os.Getenv("db_name")
 	dbHost := os.Getenv("db_host")
 
 	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password) //Build connection string
+	fmt.Println(dbUri)
 
 	conn, err := gorm.Open("postgres", dbUri)
 	if err != nil {
@@ -47,38 +36,25 @@ _  __  / / /_/ / / /_/ / __/ /  _  /|  /
 
 	db = conn
 
-	// db.DropTableIfExists(&model.Devices{}, &model.Data_Types{}, model.Data_Stored{})
-	db.AutoMigrate(&model.Devices{}, &model.Data_Types{}, &model.Data_Stored{})
-
-	parser.SetDatabaseConnection(db)
+	db.DropTable(&Devices{}, &Data_Types{}, Data_Stored{})
+	db.AutoMigrate(&Devices{}, &Data_Types{}, &Data_Stored{})
 
 	mqttConnect()
 }
 
-//callback for when a message is recieved from the broker
 var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-	fmt.Printf("---> [%s] received message: \n%s\n---> from topic: %s\n",
-		time.Now().Format("2006-01-02 15:04:05"),
-		msg.Payload(),
-		msg.Topic())
-
-	go parser.SplitStore(msg.Payload())
+	fmt.Printf("received message : %s from topic: %s\n", msg.Payload(), msg.Topic())
+	go splitStore(msg.Payload())
 }
 
-//callback for when the client is connected to the broker
 var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
 	fmt.Println("Connected")
 }
 
-//callback for when the connection to the broker is lost
-//this wil kill the application, this is intended for use with docker
 var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
-	fmt.Printf("--->[%s] Connection to mqtt broker lost, shutting down",
-		time.Now().Format("2006-01-02 15:04:05"))
-	os.Exit(1)
+	fmt.Printf("Connection lost: %v", err)
 }
 
-// mqttConnect configures the mqtt client and connect to the broker
 func mqttConnect() {
 
 	e := godotenv.Load() //Load .env file
@@ -86,15 +62,13 @@ func mqttConnect() {
 		fmt.Print(e)
 	}
 
-	//gets the connect variables from the .env file
 	user := os.Getenv("mqtt_user")
 	url := os.Getenv("mqtt_url")
 	pass := os.Getenv("mqtt_pass")
 	feed := os.Getenv("mqtt_feed")
 
-	fmt.Printf("---> [%s] Starting client\n", time.Now().Format("2006-01-02 15:04:05"))
+	fmt.Println("Starting client")
 
-	//creates the connect options for the mqtt client
 	opts := mqtt.NewClientOptions()
 	opts.AddBroker(url)
 	opts.SetUsername(user)
@@ -116,12 +90,11 @@ func main() {
 	<-c
 }
 
-// subscribe subscribes to the mqtt feed specified in the .env file
 func subscribe(client mqtt.Client, feed string) {
 	topic := feed
 
 	token := client.Subscribe(topic, 1, nil)
 
 	token.Wait()
-	fmt.Printf("Subscribed to: %s\n", topic)
+	fmt.Printf("Subscribed to: %s", topic)
 }
