dfdc049fff73dc34a13ee5d3bebc6ce80067909b
Heart <heartbrrrrr@protonmail.com>
2021-05-25 09:30:17 +0200

    Add author too all sensor files

--- Embedded/src/sensors/Clock.cpp
+++ Embedded/src/sensors/Clock.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file    WeatherData.hpp
+ * @author  Linnéa Mörk
+ * @version 0.1.0
+ * @date    2021-04-12
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
 #include "Clock.hpp"
 
 RTC_PCF8523 rtc;
